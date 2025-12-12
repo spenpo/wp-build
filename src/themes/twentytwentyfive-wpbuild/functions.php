39dc66dd7cabaaf7d70c6cc3a72e89a6c372209a
Spencer Pope <spenpo@spenpo.com>
2025-12-12 11:15:19 +0800

    add stylesheet to child theme

--- src/themes/twentytwentyfive-wpbuild/functions.php
+++ src/themes/twentytwentyfive-wpbuild/functions.php
@@ -9,3 +9,18 @@
  * @since 1.0.0
  * @version 1.0.0
  */
+
+ add_action('wp_enqueue_scripts', function() {
+    // Enqueue parent theme styles first
+    wp_enqueue_style(
+        'twentytwentyfive-style',
+        get_template_directory_uri() . '/style.css'
+    );
+    
+    // Enqueue child theme's main style.css
+    wp_enqueue_style(
+        'twentytwentyfive-wpbuild-style',
+        get_stylesheet_uri(),
+        ['twentytwentyfive-wpbuild-style']
+    );
+});
