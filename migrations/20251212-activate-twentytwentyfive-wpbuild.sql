ae3725286e9e370b1a4fd80f69c870523def6019
Spencer Pope <spenpo@spenpo.com>
2025-12-12 10:44:12 +0800

    activate child theme

---
+++ migrations/20251212-activate-twentytwentyfive-wpbuild.sql
@@ -0,0 +1,6 @@
+-- Migration: activate-twentytwentyfive-wpbuild
+-- Date: 2025-12-12
+-- Sequence: none (first migration of the day)
+
+-- Add your SQL migration code here
+
---
+++ src/themes/twentytwentyfive-wpbuild/functions.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Twenty Twenty Five WPBuild Theme Functions
+ * 
+ * This file contains the functions for the Twenty Twenty Five WPBuild theme.
+ * 
+ * @package Twenty_Twenty_Five_WPBuild
+ * @subpackage Functions
+ * @since 1.0.0
+ * @version 1.0.0
+ */
